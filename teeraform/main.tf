375a73f24690e088bf3fa6f6ff9c7158c2a9b300
murrali krishna <pudivikash@gmail.com>
2022-10-14 23:07:04 -0700

    added teraform templates

---
+++ teeraform/main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_resource_group" "topname" {
+  name     = "inresourcegrop"
+  location = "eastus"
+}
+resource "azurerm_virtual_network" "main" {
+  name                 = "${var.prefix}-network"
+  address_space        = ["10.0.0.0/16"]
+  location             =  "eastus"
+  resource_group_name  = inresourcegrop
+}

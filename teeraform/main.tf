157a51edf1c78e8b5b835ba966ab6f5dab991e83
murrali krishna <pudivikash@gmail.com>
2022-10-14 23:43:55 -0700

    fix

--- teeraform/main.tf
+++ teeraform/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "topname" {
   location = "eastus"
 }
 resource "azurerm_virtual_network" "main" {
-  name                 = "${var.prefix}-network"
+  name                 = "vnet"
   address_space        = ["10.0.0.0/16"]
   location             =  "eastus"
   resource_group_name  = inresourcegrop
